42b7743a01578f77b75ed955915bf0e39ef6692d
Doug <djmacdonald@uchicago.edu>
2015-03-05 15:09:59 -0500

    added basic structure of movies controller

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,23 @@
+class MoviesController < ApplicationController
+  def index
+
+  end
+
+  def show
+
+  end
+
+  def create
+
+  end
+
+  def update
+
+  end
+
+  private
+
+  def movie_params
+
+  end
+end
