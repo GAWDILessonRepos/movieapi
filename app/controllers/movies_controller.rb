a4e9742978dcb5c1e1bf25323ed400f3f48f5d76
Doug <djmacdonald@uchicago.edu>
2015-03-05 15:17:16 -0500

    filled in all methods for movie controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,23 +1,35 @@
 class MoviesController < ApplicationController
   def index
-
+    @movies = Movie.all
+    render json: @movies, status: 200
   end
 
   def show
-
+    @movie = Movie.find(params[:id])
+    render json: @movie, status: 200
   end
 
   def create
-
+    @movie = Movie.new(movie_params)
+    if @movie.save
+      render json: @movie, status: :created, location: @movie
+    else
+      render json: @movie.errors, status: :unprocessable_entity
+    end
   end
 
   def update
-
+    @movie = Movie.find(params[:id])
+    if @movie.update(movie_params)
+      render json: @movie, status: :ok
+    else
+      render json: @movie.errors, status: :unprocessable_entity
+    end
   end
 
   private
 
   def movie_params
-
+    params.require(:movie).permit(:title, :gross, :release, :rating, :description)
   end
 end
