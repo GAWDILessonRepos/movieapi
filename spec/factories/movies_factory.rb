a12d01aa2b16416a73e6ae8b4a86b594c4b55078
Doug <djmacdonald@uchicago.edu>
2015-03-05 16:01:22 -0500

    changed release to a date

--- spec/factories/movies_factory.rb
+++ spec/factories/movies_factory.rb
@@ -3,7 +3,7 @@ FactoryGirl.define do
   factory :movie do
     title Faker::Lorem.sentence
     gross Faker::Number.number(8)
-    release Faker::Number.number(4)
+    release Faker::Date.between(200.days.ago, Date.today)
     rating Faker::Lorem.word
     description Faker::Lorem.paragraph(2)
   end
