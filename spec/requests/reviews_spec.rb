e35c82fb502140c81157312bad9cdcbddcf1a932
Doug <djmacdonald@uchicago.edu>
2015-03-05 16:18:22 -0500

    updated urls

--- spec/requests/reviews_spec.rb
+++ spec/requests/reviews_spec.rb
@@ -13,17 +13,18 @@ describe 'Comments Requests' do
   end
   describe "#index" do
     it 'gets all of the comments for a post' do
-      get "/posts/#{@post.id}/comments"
+      get "/movies/#{@movie.id}/reviews"
       expect(response).to be_success
       json = JSON.parse(response.body)
     end
   end
   describe '#create' do
     it 'should create a comment on that post and return the comment' do
-      post "/posts/#{@posts.first.id}/comments",
-      { comment: {
-          body: "Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Donec sed odio dui.",
-          author: "JSONWharff"
+      post "/movies/#{@movieF.id}/comments",
+      { review: {
+          comment: "Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Donec sed odio dui.",
+          stars: 4,
+          reviewer: "JSONWharff"
         } }.to_json,
       { 'Accept' => Mime::JSON, 'Content-Type' => Mime::JSON.to_s }
       expect(response).to be_success
