a224ef929a19284ac9d0b62bef8ca600dd3f9130
abroccoli <acbrocco@syr.edu>
2015-03-05 15:24:03 -0500

    wrote index and create controllers for reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,23 @@
 class ReviewsController < ApplicationController
-	
+	def index
+		@reviews = Movie.find(params[:movie_id]).reviews
+		render json: @reviews, status: 200
+	end
+
+	def create
+		@review = Movie.find(params[:movie_id]).reviews.new(review_params)
+		if @review.save
+			render json: @review, status: 201
+		else
+			render json: @review.errors, status: :unprocessable_entity
+		end
+	end
+
+	private
+
+	def review_params
+		params.require(:review).permit(:comment, :stars, :reviewer)
+	end
 
 
 end
