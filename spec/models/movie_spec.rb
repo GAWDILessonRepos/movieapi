89984929046f00a559da14398e66c8814629a8a4
Dan Stoppelman <dstop75@gmail.com>
2015-03-06 16:28:15 -0500

    add title uniqueness validation test

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -1,7 +1,13 @@
 # DS
 require 'rails_helper'
 
-RSpec.describe Movie do
+RSpec.describe 'Movie model' do
+  before(:all) do
+    Movie.destroy_all
+    @movies = FactoryGirl.create_list(:movie, 10)
+    @movie = @movies.first
+  end
+
   describe '.create' do
     it 'creates a new movie' do
       expect(Movie.create()).to be_a Movie
@@ -10,9 +16,27 @@ RSpec.describe Movie do
     it 'is invalid without a title' do
       expect(Movie.create(
         gross: 2000000,
-        release: Date.new(1993, 5, 12),
+        release: Date.new(2013, 5, 12),
         rating: "R",
         description: "yabba dabba do"
+        )).not_to be_valid
+    end
+
+    it 'is invalid if the title is not unique' do
+      Movie.create(
+        title: "BAZZZZZING",
+        gross: 1111111,
+        release: Date.new(2015, 12, 12),
+        rating: "R",
+        description: "yabba dabba do"
+        );
+
+      expect(Movie.create(
+        title: "BAZZZZZING",
+        gross: 2222222,
+        release: Date.new(2014, 11, 11),
+        rating: "PG-13",
+        description: "er huh huh... OK, Fred!"
         )).not_to be_valid
     end
 
