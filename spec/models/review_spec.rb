bd2b16d4066c04ef87277f0a1f04166e300a5da4
Dan Stoppelman <dstop75@gmail.com>
2015-03-06 18:28:11 -0500

    add test: Review .create is invalid without a comment

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -11,5 +11,20 @@ RSpec.describe Review do
     it 'creates a new review' do
       expect(Review.new()).to be_a Review
     end
+
+    it 'is invalid without a comment' do
+      testMovie = Movie.create(
+        title: "BAZZZZZING",
+        gross: 2000000,
+        rating: "NC-17",
+        release: Date.new(2014, 5, 12),
+        description: "yabba dabba do"
+        )
+
+      expect(testMovie.reviews.create(
+        stars: 2,
+        reviewer: 'hoss'
+        )).not_to be_valid
+    end
   end
 end
