f61554973204955a8b5ff2e4c5ebea4951dd2a94
Dan Stoppelman <dstop75@gmail.com>
2015-03-06 18:29:38 -0500

    add test: Review.create is invalid if the reviewer name is not unique to the movie

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -26,5 +26,26 @@ RSpec.describe Review do
         reviewer: 'hoss'
         )).not_to be_valid
     end
+
+    it 'is invalid if the reviewer has already reviewed this movie (if reviewer is not unique to the movie)' do
+      testMovie = Movie.create(
+        title: "BAZZZZZING",
+        gross: 2000000,
+        rating: "NC-17",
+        release: Date.new(2014, 5, 12),
+        description: "yabba dabba do"
+        )
+      testMovie.reviews.create(
+        comment: 'yay',
+        stars: 5,
+        reviewer: 'fake'
+        )
+
+      expect(testMovie.reviews.create(
+        comment: 'boo',
+        stars: 1,
+        reviewer: 'fake'
+        )).not_to be_valid
+    end
   end
 end
